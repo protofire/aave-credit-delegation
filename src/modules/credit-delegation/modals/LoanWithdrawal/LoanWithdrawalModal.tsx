45d459e60e4c77c8ec4f0fc87cc111d3128ed201
Alaa Saleh <alaaisale7@gmail.com>
2023-09-16 17:07:24 -0300

    feat(credit-delegation): hotfix

--- src/modules/credit-delegation/modals/LoanWithdrawal/LoanWithdrawalModal.tsx
+++ src/modules/credit-delegation/modals/LoanWithdrawal/LoanWithdrawalModal.tsx
@@ -4,12 +4,12 @@ import { BasicModal } from 'src/components/primitives/BasicModal';
 import { ModalWrapper } from 'src/components/transactions/FlowCommons/ModalWrapper';
 import { ModalContextType, ModalType, useModalContext } from 'src/hooks/useModal';
 
-import { CreditLine } from '../../types';
+import { ApplicationOrCreditLine } from '../../types';
 import { LoanWithdrawalModalContent } from './LoanWithdrawalModalContent';
 
 export const LoanWithdrawalModal = () => {
   const { type, close, args } = useModalContext() as ModalContextType<{
-    creditLine: CreditLine;
+    creditLine: ApplicationOrCreditLine;
   }>;
 
   return (
